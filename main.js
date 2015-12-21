18d72229bfa2672846db9b770ae1c8c679443322
Niklane <cswcsy@gmail.com>
2015-12-22 01:05:07 +0900

    programmed via webstorm

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function main(){
-	console.log("Hello Goorm!");
-}
-
-main();
+var http = require('http');
+http.createServer(function(request, response) {
+	response.writeHead(200);
+	response.write("Hello, this is dog.");
+	response.end();
+}).listen(3000);
+console.log('Listening on port 3000...');
